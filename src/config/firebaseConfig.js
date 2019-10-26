398a16b7bdf1f65913f586c1b4ca6fdef3341d36
Jadson Medeiros <jadsonufc@gmail.com>
2019-10-26 10:22:50 -0300

    UPLOAD TESTE FIREBASE

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,15 +1,14 @@
 import Firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDZk3azmOfmEIS9rVWtiPuGEwMUTL-LQPw",
-    authDomain: "finapp-29934.firebaseapp.com",
-    databaseURL: "https://finapp-29934.firebaseio.com",
-    projectId: "finapp-29934",
-    storageBucket: "finapp-29934.appspot.com",
-    messagingSenderId: "313115294998",
-    appId: "1:313115294998:web:3475f82a7d8f28a0cb2437",
-    measurementId: "G-3717YFXY8E"
-  };
+  apiKey: "AIzaSyCJpQ42gEUwhPcZURiJ8kfXv-6gXculsLU",
+  authDomain: "controle-financeiro-c115d.firebaseapp.com",
+  databaseURL: "https://controle-financeiro-c115d.firebaseio.com",
+  projectId: "controle-financeiro-c115d",
+  storageBucket: "controle-financeiro-c115d.appspot.com",
+  messagingSenderId: "809647219346",
+  appId: "1:809647219346:web:caa7c47858284edc003c4b"
+};
 
   const app = Firebase.initializeApp(firebaseConfig);
   export default db = app.database();
